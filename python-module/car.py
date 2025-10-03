75edb1381995e77402d94c576216dfa629c3f22c
Abbie-rony <ayokemiayinde@gmail.com>
2025-10-03 12:42:09 -0700

    Add files via upload

---
+++ python-module/car.py
@@ -0,0 +1,11 @@
+class car:
+    def_init_( self, brand, model):
+      self.brand = brand
+      self.model=  model
+    def drive(self)
+print("f*(self.brand) (self.model) is driving")   
+   my_car=car("honda", "mecedesbenz")    
+    
+
+    
+    
---
+++ python-module/phone.py
@@ -0,0 +1,7 @@
+class smartphone:
+    def_init_(self, version, model,):
+ self.version = version
+         self.model =speciality
+    def use(self):
+print(f*(self.version) (self.model)is use)         
+
